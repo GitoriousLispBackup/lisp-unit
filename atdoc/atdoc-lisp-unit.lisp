fcfd9abb365575160e0427481e8c9f34d473e947
Dieter Kaiser <crategus@users.sourceforge.net>
2012-10-24 22:34:58 +0200

    Adding a header

--- atdoc/atdoc-lisp-unit.lisp
+++ atdoc/atdoc-lisp-unit.lisp
@@ -1,3 +1,29 @@
+;;; ----------------------------------------------------------------------------
+;;; atdoc-lisp-unit.lisp
+;;;
+;;; Documentation strings for generating the HTML documentation for lisp-unit.
+;;;
+;;; Copyright (C) 2012 Dieter Kaiser
+;;;
+;;; Permission is hereby granted, free of charge, to any person obtaining
+;;; a copy of this software and associated documentation files (the "Software"),
+;;; to deal in the Software without restriction, including without limitation
+;;; the rights to use, copy, modify, merge, publish, distribute, sublicense,
+;;; and/or sell copies of the Software, and to permit persons to whom the
+;;; Software is furnished to do so, subject to the following conditions:
+;;;
+;;; The above copyright notice and this permission notice shall be included
+;;; in all copies or substantial portions of the Software.
+;;;
+;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+;;; OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+;;; THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+;;; OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+;;; ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+;;; OTHER DEALINGS IN THE SOFTWARE.
+;;; ----------------------------------------------------------------------------
+
 (in-package :lisp-unit)
 
 (setf (documentation (find-package :lisp-unit) t)
@@ -390,3 +416,4 @@
   @end{section}
 ")
 
+;;; --- End of file atdoc-lisp-unit.lisp ---------------------------------------
